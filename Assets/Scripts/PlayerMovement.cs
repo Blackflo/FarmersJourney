34bebf01f5884d8fadc42f2b918bde1d08ab6762
Reinsch <71188011+Blackflo@users.noreply.github.com>
2026-08-31 14:19:24 +0200

    OmnidirectionalAnimation
    Ka is immernoch verbugged, links und rechts unten springt zur idle von oben. TBD

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,7 @@ public class MovementHandler : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Animator animator;
-
-  
+    private Vector2 lastMoveDirection = Vector2.down;
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -23,17 +22,28 @@ public class MovementHandler : MonoBehaviour
         MovePlayer();
     }
 
+
+
     void MovePlayer()
     {
         Vector2 moveInput = moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime;
 
-          animator.SetFloat("MoveX", moveInput.x);
-          animator.SetFloat("MoveY", moveInput.y);
 
 
+
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+
+        
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            lastMoveDirection = moveInput;
+        }
+        animator.SetFloat("LastMoveX", lastMoveDirection.x);
+        animator.SetFloat("LastMoveY", lastMoveDirection.y);
         animator.SetFloat("Speed", moveInput.sqrMagnitude);
 
 
-}
+    }
 }
